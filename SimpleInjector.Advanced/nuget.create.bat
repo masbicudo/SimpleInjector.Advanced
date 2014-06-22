1cef95c252d55ead729f991b64295fcf4b5eaccb
masbicudo <masbicudo@gmail.com>
2014-06-22 17:48:25 -0300

    New NuGet package info.

---
+++ SimpleInjector.Advanced/nuget.create.bat
@@ -0,0 +1,2 @@
+nuget pack SimpleInjector.Advanced.csproj -Prop Configuration=Release -Prop Platform=AnyCPU
+nuget pack SimpleInjector.Advanced.csproj -Prop Configuration=Release -Prop Platform=AnyCPU -Symbols
